80559adb2093f4548280e772017b3e19cde37fa0
Sam Smith <scs5@clemson.edu>
2023-12-16 10:07:09 -0500

    Added script for real-time inference

---
+++ src/inference.py
@@ -0,0 +1,81 @@
+import pickle
+import cv2
+import mediapipe as mp
+import numpy as np
+import utils.config as config
+
+
+def realtime_inference():
+    """ Predicts signs in real-time using camera. """
+    model_dict = pickle.load(open(config.MODEL_FN, 'rb'))
+    model = model_dict['model']
+
+    mp_hands = mp.solutions.hands
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    while True:
+        hand_features = []
+        x_positions = []
+        y_positions = []
+        ret, frame = cap.read()
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Skip iteration if 0 or 2 hands are seen
+        results = hands.process(frame_rgb)
+        if not results.multi_hand_landmarks or len(results.multi_hand_landmarks) != 1:
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
+            continue
+
+        # Draw hand marks on camera
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                frame,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+
+        # Add hand marks to features
+        for hand_landmarks in results.multi_hand_landmarks:
+            for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+                x_positions.append(x)
+                y_positions.append(y)
+
+                hand_features.append(x)
+                hand_features.append(y)
+
+        # Form bounding box of hand marks
+        H, W, _ = frame.shape
+        x1 = int(min(x_positions) * W) - 10
+        y1 = int(min(y_positions) * H) - 10
+        x2 = int(max(x_positions) * W) + 10
+        y2 = int(max(y_positions) * H) + 10
+
+        # Predict sign
+        prediction = model.predict([np.asarray(hand_features)])
+        predicted_character = prediction[0]
+
+        # Display on camera
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
+                    cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+
+        # Exit on escape
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    realtime_inference()
